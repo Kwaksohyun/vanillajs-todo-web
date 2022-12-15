265e50965742978f9c42466eba665f9e801886e1
Kwaksohyun <73460505+Kwaksohyun@users.noreply.github.com>
2022-12-15 19:07:58 +0900

    Update calendar and clock layout

--- js/calendar.js
+++ js/calendar.js
@@ -4,16 +4,16 @@ const calWeekDay = document.querySelector(".cal-wrap__weekday");
 const calWrapDates = document.querySelector(".cal-wrap__dates");
 
 function showCalendar() {
-    const todayDate = new Date();
-    const year = todayDate.getFullYear();
-    const month = String(todayDate.getMonth()+1);
+    const todayD = new Date();
+    const year = todayD.getFullYear();
+    const month = String(todayD.getMonth()+1);
     const weekday = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
     // 이번 달의 첫째 날 요일
-    const firstDay = new Date(todayDate.getFullYear(), todayDate.getMonth(), 1).getDay();
+    const firstDay = new Date(todayD.getFullYear(), todayD.getMonth(), 1).getDay();
 
     // 이번 달의 마지막 날, 마지막 날짜
-    const lastDay = new Date(todayDate.getFullYear(), todayDate.getMonth()+1, 0);
+    const lastDay = new Date(todayD.getFullYear(), todayD.getMonth()+1, 0);
     const lastDate = lastDay.getDate();
 
     calMonth.innerText = month;
--- js/clock.js
+++ js/clock.js
@@ -1,22 +1,24 @@
-const clock = document.querySelector("h2#clock");
-const todaysDate = document.querySelector("h2#todays-date");
+const clock = document.querySelector("h2.clock");
+const todaysDay = document.querySelector("h1#todays-day");
+const todaysDate = document.querySelector("h1#todays-date");
 
 function getClock() {
-    const todayDate = new Date();
+    const todayD = new Date();
 
-    const year = todayDate.getFullYear();
-    const month = String(todayDate.getMonth()+1);
-    const date = Math.floor(todayDate.getDate());
+    const year = todayD.getFullYear();
+    const month = String(todayD.getMonth()+1);
+    const date = Math.floor(todayD.getDate());
     
     const weekday = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-    const day = weekday[todayDate.getDay()];
+    const day = weekday[todayD.getDay()];
 
-    const hours = String(todayDate.getHours()).padStart(2, "0");
-    const minutes = String(todayDate.getMinutes()).padStart(2, "0");
-    const seconds = String(todayDate.getSeconds()).padStart(2, "0");
+    const hours = String(todayD.getHours()).padStart(2, "0");
+    const minutes = String(todayD.getMinutes()).padStart(2, "0");
+    const seconds = String(todayD.getSeconds()).padStart(2, "0");
     
     clock.innerText = `${hours}:${minutes}:${seconds}`;
-    todaysDate.innerText = `${year}.${month}.${date} (${day})`;
+    todaysDay.innerText = `${day}`
+    todaysDate.innerText = `${year}.${month}.${date}`;
 }
 
 getClock();                     // 웹사이트가 load되자마자 function 실행(즉시 호출)
